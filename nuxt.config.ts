d14a948841621256c5998b44a953f32915b87f7c
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-02-13 20:41:22 +0500

    Logo, Meta description/title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,30 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon'
-  ]
+  ],
+  app: {
+    head: {
+      title: "Nuxt Starter Components",
+      link: [
+        {
+          rel: "shortcut icon",
+          href: "/logo.png"
+        }
+      ],
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          name: "description",
+          content: "Starter Nuxt components so you don't have to create them from scratch every time!"
+        }
+      ]
+    }
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
